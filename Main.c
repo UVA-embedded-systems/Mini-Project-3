0d02d504e281ac68fa649d669883944c8b9174e5
Homa Alemzadeh <homa.alem@gmail.com>
2025-02-28 14:22:29 -0500

    Adding mini project files

---
+++ Main.c
@@ -0,0 +1,294 @@
+// Main.c
+// Runs on LM4F120/TM4C123
+// You may use, edit, run or distribute this file 
+// You are free to change the syntax/organization of this file
+
+// Jonathan W. Valvano 2/20/17, valvano@mail.utexas.edu
+// Modified by Sile Shu 10/4/17, ss5de@virginia.edu
+// Modified by Mustafa Hotaki 7/29/18, mkh3cf@virginia.edu
+
+#include <stdint.h>
+#include "OS.h"
+#include "tm4c123gh6pm.h"
+#include "LCD.h"
+#include <string.h> 
+#include "UART.h"
+#include "FIFO.h"
+#include "joystick.h"
+#include "PORTE.h"
+
+// Constants
+#define BGCOLOR     					LCD_BLACK
+#define CROSSSIZE            	5
+#define PERIOD               	4000000   // DAS 20Hz sampling period in system time units
+#define PSEUDOPERIOD         	8000000
+#define LIFETIME             	1000
+#define RUNLENGTH            	600 // 30 seconds run length
+
+extern Sema4Type LCDFree;
+uint16_t origin[2]; 	// The original ADC value of x,y if the joystick is not touched, used as reference
+int16_t x = 63;  			// horizontal position of the crosshair, initially 63
+int16_t y = 63;  			// vertical position of the crosshair, initially 63
+int16_t prevx, prevy;	// Previous x and y values of the crosshair
+uint8_t select;  			// joystick push
+uint8_t area[2];
+uint32_t PseudoCount;
+
+unsigned long NumCreated;   		// Number of foreground threads created (OS_Kill does not decrement this)
+unsigned long NumSamples;   		// Incremented every ADC sample, in Producer
+unsigned long UpdateWork;   		// Incremented every update on position values
+unsigned long Calculation;  		// Incremented every cube number calculation
+
+//---------------------User debugging-----------------------
+unsigned long DataLost;     // data sent by Producer, but not received by Consumer
+long MaxJitter;             // largest time jitter between interrupts in usec
+#define JITTERSIZE 64
+unsigned long const JitterSize=JITTERSIZE;
+unsigned long JitterHistogram[JITTERSIZE]={0,};
+unsigned long TotalWithI1;
+unsigned short MaxWithI1;
+
+void Device_Init(void){
+	UART_Init();
+	BSP_LCD_OutputInit();
+	BSP_Joystick_Init();
+}
+//------------------Task 1--------------------------------
+// background thread executed at 20 Hz
+//******** Producer *************** 
+int UpdatePosition(uint16_t rawx, uint16_t rawy, jsDataType* data){
+	if (rawx > origin[0]){
+		x = x + ((rawx - origin[0]) >> 9);
+	}
+	else{
+		x = x - ((origin[0] - rawx) >> 9);
+	}
+	if (rawy < origin[1]){
+		y = y + ((origin[1] - rawy) >> 9);
+	}
+	else{
+		y = y - ((rawy - origin[1]) >> 9);
+	}
+	if (x > 127){
+		x = 127;}
+	if (x < 0){
+		x = 0;}
+	if (y > 120 - CROSSSIZE){
+		y = 120 - CROSSSIZE;}
+	if (y < 0){
+		y = 0;}
+	data->x = x; data->y = y;
+	return 1;
+}
+
+void Producer(void){
+	uint16_t rawX,rawY; // raw adc value
+	uint8_t select;
+	jsDataType data;
+	unsigned static long LastTime;  // time at previous ADC sample
+	unsigned long thisTime;         // time at current ADC sample
+	long jitter;                    // time between measured and expected, in us
+	if (NumSamples < RUNLENGTH){
+		BSP_Joystick_Input(&rawX,&rawY,&select);
+		thisTime = OS_Time();       // current time, 12.5 ns
+		UpdateWork += UpdatePosition(rawX,rawY,&data); // calculation work
+		NumSamples++;               // number of samples
+		if(JsFifo_Put(data) == 0){ // send to consumer
+			DataLost++;
+		}
+	//calculate jitter
+		if(UpdateWork > 1){    // ignore timing of first interrupt
+			unsigned long diff = OS_TimeDifference(LastTime,thisTime);
+			if(diff > PERIOD){
+				jitter = (diff-PERIOD+4)/8;  // in 0.1 usec
+			}
+			else{
+				jitter = (PERIOD-diff+4)/8;  // in 0.1 usec
+			}
+			if(jitter > MaxJitter){
+				MaxJitter = jitter; // in usec
+			}       // jitter should be 0
+			if(jitter >= JitterSize){
+				jitter = JITTERSIZE-1;
+			}
+			JitterHistogram[jitter]++; 
+		}
+		LastTime = thisTime;
+	}
+}
+
+//--------------end of Task 1-----------------------------
+
+//------------------Task 2--------------------------------
+// background thread executes with SW1 button
+// one foreground task created with button push
+// foreground treads run for 1 sec and die
+// ***********ButtonWork*************
+void ButtonWork(void){
+	uint32_t StartTime,CurrentTime,ElapsedTime;
+	StartTime = OS_MsTime();
+	ElapsedTime = 0;
+	OS_bWait(&LCDFree);
+	BSP_LCD_FillScreen(BGCOLOR);
+	while (ElapsedTime < LIFETIME){
+
+		CurrentTime = OS_MsTime();
+		ElapsedTime = CurrentTime - StartTime;
+		BSP_LCD_Message(0,5,0,"Life Time:",LIFETIME);
+		BSP_LCD_Message(1,0,0,"Horizontal Area:",area[0]);
+		BSP_LCD_Message(1,1,0,"Vertical Area:",area[1]);
+		BSP_LCD_Message(1,2,0,"Elapsed Time:",ElapsedTime);
+		OS_Sleep(50);
+	}
+	BSP_LCD_FillScreen(BGCOLOR);
+	OS_bSignal(&LCDFree);
+  OS_Kill();  // done, OS does not return from a Kill
+} 
+
+//************SW1Push*************
+// Called when SW1 Button pushed
+// Adds another foreground task
+// background threads execute once and return
+void SW1Push(void){
+  if(OS_MsTime() > 20 ){ // debounce
+    if(OS_AddThread(&ButtonWork,128,4)){
+      NumCreated++; 
+    }
+    OS_ClearMsTime();  // at least 20ms between touches
+  }
+}
+
+//--------------end of Task 2-----------------------------
+
+//------------------Task 3--------------------------------
+
+//******** Consumer *************** 
+// foreground thread, accepts data from producer
+// Display crosshair and its positions
+// inputs:  none
+// outputs: none
+void Consumer(void){
+	while(NumSamples < RUNLENGTH){
+		jsDataType data;
+		JsFifo_Get(&data);
+		OS_bWait(&LCDFree);
+			
+		BSP_LCD_DrawCrosshair(prevx, prevy, LCD_BLACK); // Draw a black crosshair
+		BSP_LCD_DrawCrosshair(data.x, data.y, LCD_RED); // Draw a red crosshair
+
+		BSP_LCD_Message(1, 5, 3, "X: ", x);		
+		BSP_LCD_Message(1, 5, 12, "Y: ", y);
+		OS_bSignal(&LCDFree);
+		prevx = data.x; 
+		prevy = data.y;
+	}
+  OS_Kill();  // done
+}
+
+
+//--------------end of Task 3-----------------------------
+
+//------------------Task 4--------------------------------
+// foreground thread that runs without waiting or sleeping
+// it executes some calculation related to the position of crosshair 
+//******** CubeNumCalc *************** 
+// foreground thread, calculates the virtual cube number for the crosshair
+// never blocks, never sleeps, never dies
+// inputs:  none
+// outputs: none
+
+void CubeNumCalc(void){ 
+	uint16_t CurrentX,CurrentY;
+  while(1) {
+		if(NumSamples < RUNLENGTH){
+			CurrentX = x; CurrentY = y;
+			area[0] = CurrentX / 22;
+			area[1] = CurrentY / 20;
+			Calculation++;
+		}
+  }
+}
+//--------------end of Task 4-----------------------------
+
+//------------------Task 5--------------------------------
+// UART background ISR performs serial input/output
+// Two software fifos are used to pass I/O data to foreground
+// Interpreter is a foreground thread, accepts input from serial port, outputs to serial port
+// inputs:  none
+// outputs: none
+// there are following commands
+//    print performance measures 
+//    time-jitter, number of data points lost, number of calculations performed
+//    i.e., NumSamples, NumCreated, MaxJitter, DataLost, UpdateWork, Calculations
+void Interpreter(void){
+	char command[80];
+  while(1){
+    OutCRLF(); UART_OutString(">>");
+		UART_InString(command,79);
+		OutCRLF();
+		if (!(strcmp(command,"NumSamples"))){
+			UART_OutString("NumSamples: ");
+			UART_OutUDec(NumSamples);
+		}
+		else if (!(strcmp(command,"NumCreated"))){
+			UART_OutString("NumCreated: ");
+			UART_OutUDec(NumCreated);
+		}
+		else if (!(strcmp(command,"MaxJitter"))){
+			UART_OutString("MaxJitter: ");
+			UART_OutUDec(MaxJitter);
+		}
+		else if (!(strcmp(command,"DataLost"))){
+			UART_OutString("DataLost: ");
+			UART_OutUDec(DataLost);
+		}
+		else if (!(strcmp(command,"UpdateWork"))){
+			UART_OutString("UpdateWork: ");
+			UART_OutUDec(UpdateWork);
+		}
+	  else if (!(strcmp(command,"Calculations"))){
+			UART_OutString("Calculations: ");
+			UART_OutUDec(Calculation);
+		}
+		else if (!(strcmp(command,"FifoSize"))){
+			UART_OutString("JSFifoSize: ");
+			UART_OutUDec(JSFIFOSIZE);
+		}
+		else{
+			UART_OutString("Command incorrect!");
+		}
+  }
+}
+//--------------end of Task 5-----------------------------
+
+
+void CrossHair_Init(void){
+	BSP_LCD_FillScreen(BGCOLOR);
+	BSP_Joystick_Input(&origin[0],&origin[1],&select);
+}
+
+//******************* Main Function**********
+int main(void){ 
+  OS_Init();           // initialize, disable interrupts
+	Device_Init();
+  CrossHair_Init();
+  DataLost = 0;        // lost data between producer and consumer
+  NumSamples = 0;
+  MaxJitter = 0;       // in 1us units
+
+//********initialize communication channels
+  JsFifo_Init();
+
+//*******attach background tasks***********
+  OS_AddSW1Task(&SW1Push,2);
+  OS_AddPeriodicThread(&Producer,PERIOD,1); // 2 kHz real time sampling of PD3
+	
+  NumCreated = 0 ;
+// create initial foreground threads
+  NumCreated += OS_AddThread(&Interpreter, 128,2); 
+  NumCreated += OS_AddThread(&Consumer, 128,1); 
+	NumCreated += OS_AddThread(&CubeNumCalc, 128,1); 
+ 
+  OS_Launch(TIME_2MS); // doesn't return, interrupts enabled in here
+	return 0;            // this never executes
+}
